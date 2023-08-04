a24cb0aff233bcab500d274c7f9d62c175cd6862
roAscurra <rominaascurra4887@gmail.com>
2023-08-04 10:49:11 -0300

    Definiendo variables para manejar el DOM

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let sonidoBatman = document.querySelector(".bati_encendido");
+let sonidoBoton = document.querySelector(".bati_click");
+
+let batman = document.querySelector(".batman");
+let batmanBoton = document.querySelector(".bati_boton");
+
+let estadoLinterna = "apagado";
